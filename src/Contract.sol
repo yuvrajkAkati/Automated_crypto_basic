e749497e9e4bc634ebbc4b9b2f61d1f9e53f6b10
yuvrajkAkati <139755961+yuvrajkAkati@users.noreply.github.com>
2025-02-27 02:38:29 +0530

    another smart contract added

--- src/Contract.sol
+++ src/Contract.sol
@@ -19,9 +19,9 @@ contract Contract {
         uint amountIn,
         uint amountOutMin,
         address recipient
-    ) external {
-        require(IERC20(tokenIn).transferFrom(msg.sender,address(this),amountIn) , "transfer failed");
-        require(IERC20(tokenIn).approve(UNISWAP_V3_ROUTER,amountIn) , "approval failed");
+    ) external returns (uint256 amountOut) {
+        IERC20(tokenIn).transferFrom(msg.sender,address(this),amountIn);
+        IERC20(tokenIn).approve(UNISWAP_V3_ROUTER,amountIn) 
 
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
             tokenIn : tokenIn,
@@ -34,10 +34,63 @@ contract Contract {
             sqrtPriceLimitX96 : 0
         });
 
-        swapRouter.exactInputSingle(params);
-
-    }
-    function logBalanceOfUser() external view {
-        
+        amountOut = swapRouter.exactInputSingle(params);
+        return amountOut;
     }
 }
+
+
+
+// //// SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.16;
+
+// import 'openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+// contract CustomToken is ERC20 {
+//     constructor(string memory name , string memory symbol) ERC20(name ,symbol){
+//         _mint(msg.sender , 10000 * 10 ** 18);
+//     }
+// }
+
+// contract Uniswap {
+//     string[] public tokens = ['CoinA' , 'CoinB' , 'CoinC'];
+//     mapping (string => ERC20) public tokenInstanceMap;
+//     uint ethValue = 100000000000000;
+
+//     constructor() {
+//         for (uint i = 0; i < tokens.length; i++) 
+//         {   
+//             CustomToken token = new CustomToken(tokens[i],tokens[i]);            
+//             tokenInstanceMap[tokens[i]] = token;
+//         }
+//     }
+
+//     function getBalance(string memory tokenName , address _address) public view returns(uint) {
+//         return tokenInstanceMap[tokenName].balanceOf(_address);
+//     }
+
+//     function getName(string memory tokenName) public view returns(string memory) {
+//         return tokenInstanceMap[tokenName].name();
+//     }
+
+//     function getTokenAddress(string memory tokenName) public view returns(address){
+//         return address(tokenInstanceMap[tokenName]);
+//     }
+
+//     function swapEthToToken(string memory tokenName) public payable returns(uint) {
+//         uint inputValue = msg.value;
+//         uint outputValue = ( inputValue / ethValue) * 10 ** 18;
+//         require(tokenInstanceMap[tokenName].transfer(msg.sender,outputValue));
+//         return outputValue;
+//     }
+
+//     function swapTokenToEth(string memory tokenName,uint _amount) public payable returns (uint) {
+//         uint exactAmount = _amount/ 10 ** 18;
+//         uint ethToBeTransferred = exactAmount * ethValue;
+//         require(address(this).balance >= ethToBeTransferred , "dex has low eth");
+//         payable(msg.sender).transfer(ethToBeTransferred);
+//         require(tokenInstanceMap[tokenName].transferFrom(msg.sender,address(this),_amount));
+//         return ethToBeTransferred;
+//     }
+
+// }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -12,19 +12,24 @@ contract TestContract is Test {
     address tokenIn;
     address tokenOut;
     address user;
-    address myAddress = 0xA4e99114eF4c26Ea93C6fB4678a4172941951706;
+    address myAddress = 0xA4e99114eF4c26Ea93C6fB4678a4172941951706; //for tests
     address private constant UNISWAP_V3_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
 
     function setUp() public {
         c = new Contract();
-        tokenIn = 0x3e622317f8C93f7328350cF0B56d9eD4C620C5d6;
-        tokenOut = 	0xCe289Bb9FB0A9591317981223cbE33D5dc42268e;
+        tokenIn = 0x3e622317f8C93f7328350cF0B56d9eD4C620C5d6; //weth
+        tokenOut = 	0xCe289Bb9FB0A9591317981223cbE33D5dc42268e; //
+        deal(address(tokenOut), address(this), 10 * 1e18);
+        deal(address(tokenIn) , address(this) , 10 * 1e18);
     }
 
-    function testFailSwap() public {  
-        uint256 balanceAfter = myAddress.balance;
-        console.log(balanceAfter);
-        assertEq(0, balanceAfter , "ok");
+    function testSwap() public {
+        // uint balance = IERC20(0x3e622317f8C93f7328350cF0B56d9eD4C620C5d6).balanceOf(myAddress); //will not work because sepoliaETH is the native token
+        c.swapTokens(tokenIn,tokenOut,300,10000000000000000,0,myAddress);
+        uint balance = IERC20(0xCe289Bb9FB0A9591317981223cbE33D5dc42268e).balanceOf(myAddress);
+        assert(balance > 0);
     }
 
+
+// 73326559626325336
 }
